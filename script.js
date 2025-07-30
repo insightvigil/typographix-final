7ecff768e78a8c230b66a5454a04065143efdf76
Adrián Vigil <insightvigil@gmail.com>
2025-07-30 15:18:34 -0600

    Home and about added

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    //Function to create and obser Intersection Observers
+    function createObserver(selector, observerOptions, toggleClass){
+        const items = document.querySelectorAll(selector);
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry=> {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add(toggleClass);
+                }
+                else {
+                    entry.target.classList.remove(toggleClass);
+                }
+            });
+        }, observerOptions)
+
+    items.forEach(item => {
+        observer.observe(item);
+        });
+    }
+
+    //Create observers for diferent sections
+    createObserver('#about .phrase', { root: null, threshold: 1}, 'active');
+});
+
+
 /*----------Navigation---------*/ 
 const nav = document.getElementById('nav');
 console.log(nav)
